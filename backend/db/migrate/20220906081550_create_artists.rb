9e04792a68d94a939eb26219682276b7e18f644d
Peter Kimani <peterjajakim@gmail.com>
2022-09-06 11:31:34 +0300

    CREATED AN ARTIST DB MIGRATION TO CREATE AN ARTIST TABLE

--- backend/db/migrate/20220906081550_create_artists.rb
+++ backend/db/migrate/20220906081550_create_artists.rb
@@ -1,4 +1,10 @@
 class CreateArtists < ActiveRecord::Migration[6.1]
   def change
+    create_table :artists do |t|
+      t.string(:name)
+      t.string(:city)
+      t.string(:image_link)
+      t.string(:genres)
+    end
   end
 end
