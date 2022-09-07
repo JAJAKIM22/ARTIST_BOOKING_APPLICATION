33488e7912e7c54c83531a403e0a00e996b41490
Peter Kimani <peterjajakim@gmail.com>
2022-09-07 14:10:29 +0300

    UPDATED ROUTES

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
       <nav class="navbar bg-light">
       <form class="container-fluid justify-content-start">
         <button class="btn btn-outline-success me-5" type="button"><Link className="nav-link" to="/artists">ARTISTS</Link></button>
-        <button class="btn btn-outline-success me-5" type="button"><Link className="nav-link" to="/VENUES">VENUES</Link></button>
+        <button class="btn btn-outline-success me-5" type="button"><Link className="nav-link" to="/venues">VENUES</Link></button>
         <button class="btn btn-outline-success me-5" type="button"><Link className="nav-link" to="/shows">SHOWS</Link></button>
         <button class="btn btn-outline-success me-5" type="button"> <Link className="nav-link" to="/forms">POSTPAGE</Link></button>
       </form>
