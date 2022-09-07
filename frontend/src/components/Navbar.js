f94b0cc099fe16148b9b229ca2f63f846070b524
Peter Kimani <peterjajakim@gmail.com>
2022-09-07 14:02:08 +0300

    STYLING THE NAVBAR

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom";
 
  export default function Navbar(){
     return(
-        <nav class="navbar navbar-dark bg-primary">
-         <Link className="nav-link" to="/artists">ARTISTS</Link>
-         <Link className="nav-link" to="/venues">VENUES</Link>
-         <Link className="nav-link" to="/shows">SHOWS</Link>
-         <Link className="nav-link" to="/forms">POSTPAGE</Link>
-       </nav>
+      <nav class="navbar bg-light">
+      <form class="container-fluid justify-content-start">
+        <button class="btn btn-outline-success me-5" type="button"><Link className="nav-link" to="/artists">ARTISTS</Link></button>
+        <button class="btn btn-outline-success me-5" type="button"><Link className="nav-link" to="/VENUES">VENUES</Link></button>
+        <button class="btn btn-outline-success me-5" type="button"><Link className="nav-link" to="/shows">SHOWS</Link></button>
+        <button class="btn btn-outline-success me-5" type="button"> <Link className="nav-link" to="/forms">POSTPAGE</Link></button>
+      </form>
+    </nav>
+         
+
               
 
     )
