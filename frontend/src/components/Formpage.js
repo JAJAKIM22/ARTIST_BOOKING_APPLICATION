70383531c7f04219cca954c4339e1dd2c00a6e5a
Peter Kimani <peterjajakim@gmail.com>
2022-09-07 13:44:38 +0300

    CREATED A SHOW AND VENUE POST FORM

--- frontend/src/components/Formpage.js
+++ frontend/src/components/Formpage.js
@@ -1,43 +1,19 @@
-import React, {useState} from "react";
-
+import React from "react";
+import NewArtistForm from "./ArtistForm";
+import NewVenueForm from "./VenueForm";
+import NewShowForm from "./ShowForm";
 function NewForm() {
-  const [name, setName] = useState("");
-  const [city, setCity] = useState("");
-  const [image_link, setImage_link] = useState("");
-  const [genres, setGenres] = useState("");
-  
-  function handleSubmit(event) {
-    const formData = {
-      name: name,
-      city: city,
-      image_link: image_link,
-      genres: genres
-    };
-    console.log(formData);
-    event.preventDefault();
-    fetch('http://localhost:9292/artists', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });}
 
     
  return (
-    <form onSubmit={handleSubmit}  class="mb-3" className="new-artist-form"> 
-     <input value={name}
-  onChange={(e) => setName(e.target.value)} placeholder="NAME" />
-     <input value={city}
-  onChange={(e) => setCity(e.target.value)} placeholder="CITY" />
-     <input value={image_link}
-  onChange={(e) => setImage_link(e.target.value)} placeholder="IMAGE_LINK" />
-     <input value={genres}
-  onChange={(e) => setGenres(e.target.value)} placeholder="Genres" />
-      <input type="submit" value="POST ARTISTS" />
-</form>
-
+    <div>
+      <NewArtistForm/>
+     <NewVenueForm/>
+     <NewShowForm/>
+    </div>
+    
 );
-}
+
+ }
 
 export default NewForm;
---
+++ frontend/src/components/ShowForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+
+
+function NewShowForm() {
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [image_link, setImage_link] = useState("");
+  
+  
+  function handleSubmit(event) {
+    const formData = {
+      name: name,
+      city: city,
+      image_link: image_link,
+    };
+    console.log(formData);
+    event.preventDefault();
+    fetch('http://localhost:9292/shows', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });}
+
+    
+ return (
+    <form onSubmit={handleSubmit}  class="mb-3" className="new-show-form"> 
+     <input value={name}
+  onChange={(e) => setName(e.target.value)} placeholder="NAME" />
+     <input value={city}
+  onChange={(e) => setCity(e.target.value)} placeholder="CITY" />
+     <input value={image_link}
+  onChange={(e) => setImage_link(e.target.value)} placeholder="IMAGE_LINK" />
+      <input type="submit" value="POST SHOWS" />
+</form>
+
+);
+}
+
+export default NewShowForm;
