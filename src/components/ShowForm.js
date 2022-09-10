42f890aad126c3ff587139e8323879aa76ac5f5c
Peter Kimani <peterjajakim@gmail.com>
2022-09-10 22:48:27 +0300

    ADDED FRONTEND FILES TO TRY AND DEPLOY THE APP

---
+++ src/components/ShowForm.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+
+
+function NewShowForm() {
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [image_link, setImage_link] = useState("");
+  const [date, setDate] = useState("");
+  
+  function handleSubmit(event) {
+    const formData = {
+      name: name,
+      city: city,
+      image_link: image_link,
+      date: date
+    };
+    console.log(formData);
+    event.preventDefault();
+    fetch('http://localhost:9292/shows', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });}
+
+    
+ return (
+  <div class= "form" >
+    <form onSubmit={handleSubmit}  class="mb-3" className="show-form"> 
+     <input value={name}
+  onChange={(e) => setName(e.target.value)} placeholder="NAME" />
+     <input value={city}
+  onChange={(e) => setCity(e.target.value)} placeholder="CITY" />
+     <input value={image_link}
+  onChange={(e) => setImage_link(e.target.value)} placeholder="IMAGE_LINK" />
+   <input value={date}
+  onChange={(e) => setDate(e.target.value)} placeholder="DATE" />
+      <input type="submit" value="POST SHOWS" />
+</form>
+</div>
+);
+}
+
+export default NewShowForm;
---
+++ src/components/VenueForm.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+
+
+function NewVenueForm() {
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [image_link, setImage_link] = useState("");
+  
+  
+  function handleSubmit(event) {
+    const formData = {
+      name: name,
+      city: city,
+      image_link: image_link,
+    };
+    console.log(formData);
+    event.preventDefault();
+    fetch('http://localhost:9292/venues', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });}
+
+    
+ return (
+  <div class= "form" >
+    <form onSubmit={handleSubmit}  class="mb-2" className="artistsform"> 
+     <input value={name}
+  onChange={(e) => setName(e.target.value)} placeholder="NAME" />
+     <input value={city}
+  onChange={(e) => setCity(e.target.value)} placeholder="CITY" />
+     <input value={image_link}
+  onChange={(e) => setImage_link(e.target.value)} placeholder="IMAGE_LINK" />
+      <input type="submit" value="POST VENUES" />
+</form>
+</div>
+);
+}
+
+export default NewVenueForm;
